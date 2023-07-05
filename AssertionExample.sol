c3d900a34a8dccf44ccae9cf4934b3f0d1abe083
Shruti Saini <124624399+ShrutiSaini07@users.noreply.github.com>
2023-07-06 02:03:39 +0530

    AssertionExample.sol

--- AssertionExample.sol
+++ AssertionExample.sol
@@ -1,24 +1,33 @@
-// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract AssertionExample {
-
-    uint256 public value;
-
-    function setValue(uint256 _newValue) public {
-
-        require(_newValue > 0, "Value must be greater than zero");
-
-        assert(_newValue != value);
-
-        value = _newValue;
+contract SmartContract {
+    address public owner;
+    
+    constructor() {
+        owner = msg.sender;
     }
-
-    function getValue() public view returns (uint256) {
-        return value;
+    
+    function withdraw(uint amount) external {
+        // require() statement to check if the caller is the contract owner
+        require(msg.sender == owner, "Only the contract owner can withdraw funds");
+        
+        // assert() statement to ensure that the amount is not greater than the contract balance
+        assert(amount <= address(this).balance);
+        
+        // Transfer the requested amount to the contract owner
+        payable(msg.sender).transfer(amount);
     }
-
-    function doRevert() public pure {
-        revert("This transaction was reverted");
+    
+    function divide(uint a, uint b) external pure returns(uint) {
+        // require() statement to ensure that the divisor is not zero
+        require(b != 0, "Divisor cannot be zero");
+        
+        // Perform the division operation and return the result
+        return a / b;
+    }
+    
+    function triggerRevert() external pure {
+        // Revert the transaction and provide a custom error message
+        revert("This transaction is explicitly reverted");
     }
 }
